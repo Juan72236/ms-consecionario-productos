0a09d5bcf8e0b582e0766418a7dc33127de17b69
JuanDeveloper <juanpablov018@gmail.com>
2021-11-27 22:03:36 -0500

    modelos ms-vendedor creados

---
+++ ms-consecionario-vendedor/src/models/index.ts
@@ -0,0 +1,3 @@
+export * from './cliente.model';
+export * from './usuario-vendedor.model';
+export * from './vendedor.model';
---
+++ ms-consecionario-vendedor/src/models/vendedor.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Vendedor extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Documento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Correo: string;
+
+
+  constructor(data?: Partial<Vendedor>) {
+    super(data);
+  }
+}
+
+export interface VendedorRelations {
+  // describe navigational properties here
+}
+
+export type VendedorWithRelations = Vendedor & VendedorRelations;
